63bc1b2b3be4307ad5bf8e0858468dea2e58b03c
jjDemas <jjdemason@comcast.net>
2021-09-10 16:03:08 -0400

    Saving code

---
+++ src/main/java/guru/springframework/sfgdi/repositories/EnglishGreetingRepositoryImpl.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgdi.repositories;
+
+public class EnglishGreetingRepositoryImpl implements EnglishGreetingRepository{
+
+    public String getGreeting() {
+        return "Hello World - EN";
+    }
+}
